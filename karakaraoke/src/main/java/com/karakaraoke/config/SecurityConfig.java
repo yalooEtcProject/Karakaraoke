89c5b396ae8992824b98649bd46df6d0b044ddd0
hwiju yeom <yeom456@gmail.com>
2023-10-16 19:25:38 +0900

    Remove: 디렉토리 구조 변경

--- karakaraoke/src/main/java/com/karakaraoke/config/SecurityConfig.java
+++ karakaraoke/src/main/java/com/karakaraoke/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.karakaraoke.webCrawler.config;
+package com.karakaraoke.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
